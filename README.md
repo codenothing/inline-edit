c9fa2f88088a5684813fa77cb78e1e503e0e4296
Corey Hart <corey@codenothing.com>
2010-03-22 00:07:29 -0700

    -Added postFormat option -Implemented $.ajax instead of $.fn.load

---
+++ README.md
@@ -0,0 +1,9 @@
+jQuery Inline Edit
+========================
+
+Enables text editing with a defined format. Checkout the [examples](http://github.com/codenothing/inline-edit/tree/master/demo/)
+for layout considerations.
+
+Credits
+--------
+[david](david@amereservant.com) - Suggesting ajax format instead of load for more control
--- src/jquery.inline-edit.js
+++ src/jquery.inline-edit.js
@@ -3,15 +3,15 @@
  * [DATE]
  * Corey Hart @ http://www.codenothing.com
  */ 
-;(function($){
-	$.fn.inlineEdit = function(options){
+(function( $, undefined ){
+	$.fn.inlineEdit = function( options ){
 		return this.each(function(){
 			// Settings
-			var $obj = $(this), 
+			var $obj = $(this), original,
 				settings = $.extend({
 					href: 'ajax.php',
 					html: true,
-					load: function(){},
+					load: undefined,
 					display: '.display',
 					form: '.form',
 					text: '.text',
@@ -20,43 +20,75 @@
 					loadtxt: 'Loading...',
 					hover: 'none-error-404',
 					postVar: 'text',
-					postData: {}
-				}, options||{}, $.metadata ? $obj.metadata() : {});
+					postData: {},
+					postFormat: undefined
+				}, options||{}, $.metadata ? $obj.metadata() : {}),
 
-			// Cache All Selectors
-			var $display = $(settings.display, $obj),
-				$form = $(settings.form, $obj),
-				$text = $(settings.text, $obj),
-				$save = $(settings.save, $obj),
-				$cancel = $(settings.cancel, $obj);
+				// Cache All Selectors
+				$display = $obj.find( settings.display ),
+				$form = $obj.find( settings.form ),
+				$text = $obj.find( settings.text ),
+				$save = $obj.find( settings.save ),
+				$cancel = $obj.find( settings.cancel );
 	
 			// Display Actions
 			$display.click(function(){
 				$display.hide();
 				$form.show();
-				if (settings.html) 
-					$text.val( $display.html() ).focus();
+
+				if ( settings.html ) {
+					if ( original === undefined ) {
+						original = $display.html();
+					}
+
+					$text.val( original ).focus();
+				}
+
 				return false;
-			}).hover(function(){ $display.addClass( settings.hover ); }, function(){ $display.removeClass( settings.hover ); });
+			})
+			.hover(function(){
+				$display.addClass( settings.hover );
+			}, function(){
+				$display.removeClass( settings.hover );
+			});
 
 			// Cancel Actions
 			$cancel.click(function(){
 				$form.hide();
 				$display.show();
 				// Remove hover action if stalled
-				if ($display.hasClass( settings.hover ))
+				if ( $display.hasClass( settings.hover ) ) {
 					$display.removeClass( settings.hover );
+				}
 				return false;
 			});
 
 			// Save Actions
 			$save.click(function(){
-				settings.postData[settings.postVar] = $text.val();
+				settings.postData[ settings.postVar ] = $text.val();
 				$form.hide();
-				$display.html(settings.loadtxt).load(settings.href, settings.postData, settings.load).show();
-				// Remove hover action if stalled
-				if ($display.hasClass( settings.hover ))
+				$display.html( settings.loadtxt ).show();
+
+				if ( $display.hasClass( settings.hover ) ) {
 					$display.removeClass( settings.hover );
+				}
+
+				$.ajax({
+					url: settings.href,
+					data: settings.dataFormat ?
+						settings.dataForm.call( $obj, {settings: settings, postData: settings.postData} ) : settings.postData,
+					success: function( response ){
+						original = undefined;
+
+						if ( settings.load ) {
+							settings.load.call( $display, response );
+							return;
+						}
+
+						$display.html( response );
+					}
+				});
+
 				return false;
 			});
 		});
